8c0143a7d62f030764843acb49b7b873b986c567
dpack1 <dylanpackham98@gmail.com>
2023-04-26 15:59:55 -0600

    Initial Commit

---
+++ MyDrink/MyDrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDrinkApp.swift
+//  MyDrink
+//
+//  Created by Dylan Packham on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MyDrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
